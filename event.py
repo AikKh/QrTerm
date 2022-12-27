2eff09daa5209e9f999f4acee86ebefc6b1b6f20
Aik Khachatrian <aik.khachatrian@gmail.com>
2022-12-27 21:51:15 +0200

    First commit

---
+++ event.py
@@ -0,0 +1,70 @@
+import qrcode
+import datetime
+from datetime import datetime as date
+
+
+class Event:
+
+    Begin = """BEGIN:VCALENDAR
+BEGIN:VEVENT
+%s
+END:VEVENT
+END:VCALENDAR"""
+
+    UID = """UID:
+DTSTART:%s
+DTEND:%s
+SUMMARY:%s
+"""
+
+    Alarm = """BEGIN:VALARM
+TRIGGER:-PT24H
+REPEAT:1
+DURATION:PT15M
+ACTION:DISPLAY
+DESCRIPTION:The expiration date has passed
+END:VALARM
+"""
+
+    def __init__(self, header: str, start_date: date, term: int, alarm = False, **params):
+        self._header = header
+
+        self._start = Event.date_to_int(start_date)
+        self._end = Event.date_to_int(start_date + datetime.timedelta(days = term))
+
+        self._alarm = alarm
+        self._params = params
+
+
+    def get_string(self):
+        uid = Event.UID % (self._start, self._end, self._header)
+
+        for key in self._params:
+            d = str(self._params[key]) + "\n"
+            uid += f"{key.upper()}:{d}"
+
+        uid = uid + Event.Alarm if self._alarm else uid
+        return Event.Begin % uid
+
+
+    def save(self, name: str):
+        "Save event as QR code"
+        name = self._header if not name else name
+        
+        img = qrcode.make(self.get_string())
+        img.save(f'{name}.png')
+
+
+    def add_param(self, key: str, value):
+        self._params[key] = value
+        
+
+    @staticmethod
+    def date_to_int(d: date):
+        days = str(d.day) if d.day >= 10 else "0" + str(d.day)
+        months = str(d.month) if d.month >= 10 else "0" + str(d.month)
+        
+        return int(str(d.year) + months + days)
+
+    
+
