1b5f677a338da3aacb9b3957a4673f3aefa35f5c
Aik Khachatrian <aik.khachatrian@gmail.com>
2022-12-27 22:09:49 +0200

    Change main

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-from to_qr import generate_event, save_qr
+from event import Event
 from datetime import datetime as date
 
 
 if __name__ == "__main__":
-    event = generate_event(
-        "Term",
-        dt = date.today(),
-        expiration_date = 90, #in days
+    event = Event(
+        header = "Term",
+        start_date = date.today(),
+        term = 90, #in days
         alarm = True,
-        # location = "United States"
-    )
-    
-    save_qr(event, "WithAlarm")
 
+        # Parametrs:
+        location = "United States"
+    )
+
+    event.save("NewEvent")
     
